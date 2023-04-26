9f87073e16cff6c0b1a96497d643707784616559
Satgonda <dattatri.satgonda@accenture.com>
2023-04-26 08:12:16 +0530

    Initial commit

---
+++ src/main/java/com/crud/aws/springbootawscrud/SpringbootAwsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud.aws.springbootawscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsCrudApplication.class, args);
+	}
+
+}

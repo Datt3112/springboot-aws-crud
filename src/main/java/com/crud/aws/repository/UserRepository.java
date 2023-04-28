c2097a576100bae76ef6fc5ddfeb7b7c62148483
Satgonda <dattatri.satgonda@accenture.com>
2023-04-28 10:15:33 +0530

    initial commit

--- src/main/java/com/crud/aws/repository/UserRepository.java
+++ src/main/java/com/crud/aws/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.crud.aws.repository;public interface UserRepository {
+package com.crud.aws.repository;
+
+import com.crud.aws.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
